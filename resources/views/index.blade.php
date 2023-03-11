928f4be834238b6715749f3ca9cf37a0afcde71b
Deivison de alcantara santos <deivison-alcantara@hotmail.com>
2023-03-11 00:14:29 -0300

    commit inicial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Primeiro deploy </title>
+</head>
+<body>
+    <p><h3>Deivison de Alcantara santos</h3></p>
+</body>
+</html>
